50beb6ae6b7f4fb87a6543b015a673a328e58788
Marc Kriguer <marc.kriguer@swirldslabs.com>
2022-08-19 13:43:08 -0700

    Add handler for "getTokenById" API method.
    Signed-off-by: Marc Kriguer <marc.kriguer@swirldslabs.com>

--- src/main/java/com/swirlds/recordserver/TokensService.java
+++ src/main/java/com/swirlds/recordserver/TokensService.java
@@ -1,6 +1,7 @@
 package com.swirlds.recordserver;
 
 import com.swirlds.recordserver.util.QueryParamUtil;
+import io.helidon.common.http.Http.Status;
 import io.helidon.config.Config;
 import io.helidon.metrics.api.RegistryFactory;
 import io.helidon.webserver.Routing;
@@ -10,6 +11,7 @@ import io.helidon.webserver.Service;
 import jakarta.json.Json;
 import jakarta.json.JsonArrayBuilder;
 import jakarta.json.JsonBuilderFactory;
+import jakarta.json.JsonNumber;
 import jakarta.json.JsonObject;
 import jakarta.json.JsonObjectBuilder;
 import org.apache.pinot.client.Connection;
@@ -52,10 +54,11 @@ public class TokensService implements Service {
      */
     @Override
     public void update(Routing.Rules rules) {
-        rules.get("/", this::getDefaultMessageHandler);
+        rules.get("/", this::listTokens);
+        rules.get("/{tokenId}", this::getTokenById);
     }
 
-    private void getDefaultMessageHandler(ServerRequest request, ServerResponse response) {
+    private void listTokens(ServerRequest request, ServerResponse response) {
         final Optional<String> publicKeyQueryParam = request.queryParams().first("publickey");
         final Optional<String> tokenIdQueryParam = request.queryParams().first("token.id");
         final Optional<String> tokenTypesQueryParam = request.queryParams().first("type"); // TODO (MYK): can be a list
@@ -90,7 +93,7 @@ public class TokensService implements Service {
                 "select consensus_timestamp, entity_number, evm_address, alias, public_key_type, public_key, fields " +
                 "from entity " + whereClause + " order by consensus_timestamp " + direction + " limit ?";
         System.out.println("queryString = " + queryString);
-        final PreparedStatement statement = this.pinotConnection.prepareStatement(new Request("sql",queryString));
+        final PreparedStatement statement = this.pinotConnection.prepareStatement(new Request("sql", queryString));
         QueryParamUtil.applyWhereClausesToQuery(whereClauses, statement);
         statement.setInt(whereClauses.size(), limit);
         final ResultSetGroup pinotResultSetGroup = statement.execute();
@@ -143,4 +146,121 @@ public class TokensService implements Service {
                         .build());
         response.send(returnObject.build());
     }
+
+/**********************
+### getTokenById not specifiying a timestamp
+GET http://localhost:8080/api/v1/tokens/107594
+### getTokenById specifiying a timestamp
+GET http://localhost:8080/api/v1/tokens/107594?timestamp=1610640452612903002
+### getTokenById specifiying an earlier timestamp
+GET http://localhost:8080/api/v1/tokens/107594?timestamp=1610640421985772001
+### getTokenById specifiying an earlier timestamp than the passed-in one
+GET http://localhost:8080/api/v1/tokens/107594?timestamp=lt:1610640421985772001
+**********************/
+    private void getTokenById(ServerRequest request, ServerResponse response) {
+        System.out.println("Calling into getTokenById!");
+	
+        final Optional<String> timestampQueryParam = request.queryParams().first("timestamp");
+
+        // build and execute query
+        final List<QueryParamUtil.WhereClause> whereClauses = new ArrayList<>();
+        // always restrict query (over all entities) to only return tokens
+        whereClauses.add(new QueryParamUtil.WhereClause(QueryParamUtil.Type._string, "type",
+                QueryParamUtil.Comparator.eq, "TOKEN"));
+        timestampQueryParam.ifPresent(s -> whereClauses.add(
+                QueryParamUtil.parseQueryString(QueryParamUtil.Type._long, "consensus_timestamp", s)));
+
+        final String whereClause = whereClauses.isEmpty() ? "" : "where " +
+                QueryParamUtil.whereClausesToQuery(whereClauses);
+        final int limit = 1; // we only want to get one token entity per API call.
+        final String queryString =
+                "select consensus_timestamp, entity_number, evm_address, alias, public_key_type, public_key, fields " +
+                "from entity " + whereClause + " order by consensus_timestamp desc limit ?";
+        System.out.println("queryString = " + queryString);
+        final PreparedStatement statement = this.pinotConnection.prepareStatement(new Request("sql", queryString));
+        QueryParamUtil.applyWhereClausesToQuery(whereClauses, statement);
+        statement.setInt(whereClauses.size(), limit);
+        final ResultSetGroup pinotResultSetGroup = statement.execute();
+        final ResultSet resultTableResultSet = pinotResultSetGroup.getResultSet(0);
+
+        // format results to JSON
+        final JsonObjectBuilder returnObject = JSON.createObjectBuilder();
+        if (resultTableResultSet.getRowCount() < 1) {
+            response.status(Status.NOT_FOUND_404);
+            response.send();
+            return;
+        }
+
+        // this shouldn't happen (because we explicitly set LIMIT to 1), but at least indicate that something is up.
+        if (resultTableResultSet.getRowCount() > 1) {
+            returnObject.add("count", resultTableResultSet.getRowCount());
+        }
+
+        final long consensusTime = resultTableResultSet.getLong(0, 0);
+        final JsonObject fields = parseFromColumn(resultTableResultSet.getString(0, 6));
+
+        addIfKeyNotNull(returnObject, "admin_key", fields, "adminKey");
+        returnObject.add("alias", resultTableResultSet.getString(0, 3));
+        addIfNotNull(returnObject, "auto_renew_account", fields.getString("autoRenewAccount", null));
+        addIfNotNull(returnObject, "auto_renew_period", fields.getString("autoRenewPeriod", null));
+        returnObject.add("created_timestamp", consensusTime);
+        // no data retained for "deleted" (boolean) field
+        addIfNotNull(returnObject, "decimals", fields.getJsonNumber("decimals"));
+        returnObject.add("evm_address", resultTableResultSet.getString(0, 2));
+        addIfNotNull(returnObject, "expiry_timestamp", fields.getJsonNumber("expiry"));
+        addIfNotNull(returnObject, "freeze_default", fields.getBoolean("freezeDefault", false));
+        addIfKeyNotNull(returnObject, "freeze_key", fields, "freezeKey");
+        addIfNotNull(returnObject, "initial_supply", fields.getString("initialSupply", null));
+        addIfKeyNotNull(returnObject, "kyc_key", fields, "kycKey");
+        addIfNotNull(returnObject, "max_supply", fields.getString("maxSupply", null));
+        addIfNotNull(returnObject, "memo", fields.getString("memo", null));
+        returnObject.add("modified_timestamp", consensusTime);
+        addIfNotNull(returnObject, "name", fields.getString("name", null));
+        addIfKeyNotNull(returnObject, "pause_key", fields, "pauseKey");
+        // no data retained for pause_status field
+        addIfNotNull(returnObject, "public_key", resultTableResultSet.getString(0, 5));
+        addIfNotNull(returnObject, "public_key_type", resultTableResultSet.getString(0, 4));
+        addIfNotNull(returnObject, "realm", fields.getString("realm", null));
+        addIfNotNull(returnObject, "shard", fields.getString("shard", null));
+        addIfKeyNotNull(returnObject, "supply_key", fields, "supplyKey");
+        addIfNotNull(returnObject, "supply_type", fields.getString("supplyType", null));
+        addIfNotNull(returnObject, "symbol", fields.getString("symbol", null));
+        returnObject.add("token_id", resultTableResultSet.getLong(0, 1));
+        // no data retained for total_supply field
+        addIfNotNull(returnObject, "treasury_account_id", fields.getString("treasury", null));
+        addIfNotNull(returnObject, "type", fields.getString("tokenType", null));
+        addIfKeyNotNull(returnObject, "wipe_key", fields, "wipeKey");
+        // no data retained for custom_fees field
+
+        response.send(returnObject.build());
+    }
+
+    private void addIfNotNull(JsonObjectBuilder object, String name, String value) {
+        if (value != null) {
+            object.add(name, value);
+        }
+    }
+
+    private void addIfNotNull(JsonObjectBuilder object, String name, JsonNumber value) {
+        if (value != null) {
+            object.add(name, value);
+        }
+    }
+
+    private void addIfNotNull(JsonObjectBuilder object, String name, Boolean value) {
+        if (value != null) {
+            object.add(name, value);
+        }
+    }
+
+    private void addIfKeyNotNull(JsonObjectBuilder object, String name, JsonObject fields, String fieldName) {
+        String value = fields.getString(fieldName, null);
+        if (value != null) {
+            object.add(name, JSON.createObjectBuilder()
+                    .add("_type", "ProtobufEncoded")
+                    .add("key", value)
+                    .build());
+        }
+    }
+
 }
